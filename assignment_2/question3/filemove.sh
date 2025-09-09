28b2332e95514ded4b749b4640f348d21f8763b0
mfonekpo <mfonekpo34@gmail.com>
2025-09-10 00:21:21 +0100

    Filemove bashscript completed

---
+++ assignment_2/question3/filemove.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "File move process initialized..."
+
+# create the json_and_csv directory
+mkdir -p json_and_CSV
+
+# move the files to the json_and_csv directory using a wildcard
+echo "moving file to designated location..."
+mv datafiles/*.{json,csv} json_and_csv/
+echo "File move process completed."
